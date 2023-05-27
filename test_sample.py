6bec5a6902311d191d90dcda2f2a69e0e433047f
Ilya Osipov <070809010@mail.ru>
2023-05-27 17:07:08 +0300

    Add basic tests example

---
+++ test_sample.py
@@ -0,0 +1,28 @@
+import pytest
+import pandas
+import json
+
+from stocks import get_yesterday_trading_prices
+from stocks import get_current_trading_prices
+
+
+def abc_1mo_dataframe():
+    with open('fixtures/abc_1mo.json') as file:
+        abc_1mo = json.load(file)
+    return pandas.DataFrame(abc_1mo)
+
+
+def abc_1d_dataframe():
+    with open('fixtures/abc_1d.json') as file:
+        abc_1d = json.load(file)
+    return pandas.DataFrame(abc_1d)
+
+
+def test_yesterday_trading_prices(mocker):
+    mocker.patch("yfinance.Ticker.history", return_value=abc_1mo_dataframe())
+    assert get_yesterday_trading_prices('ABC') == (171.5099945068, 171.0599975586)
+
+
+def test_current_trading_prices(mocker):
+    mocker.patch("yfinance.Ticker.history", return_value=abc_1d_dataframe())
+    assert get_current_trading_prices('ABC') == (170.0700073242, 169.8350067139)
