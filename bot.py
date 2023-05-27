b0569e24e83f5499aa2d131a81ab1b55094d4e36
Ilya Osipov <070809010@mail.ru>
2023-05-27 17:06:44 +0300

    Rewrite demo

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
+import datetime
 from textwrap import dedent
 
-import yfinance
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, Update, CallbackContext
 from environs import Env
+
+from stocks import get_yesterday_trading_prices
+from stocks import get_current_trading_prices
+
 
 env = Env()
 env.read_env()
@@ -15,51 +19,82 @@ portfolio = {
 }
 
 
-def start(update, context):
+def start(update: Update, context: CallbackContext) -> None:
+    chat_id = update.effective_chat.id
     update.message.reply_text('Привет! Я твой биржевой помощник!')
 
+    # This needed to exclude the situation when
+    # user presses /start multiple times and gets multiple jobs
+    job_title = f'yesterday-stocks#{chat_id}'
+    if job_title in [job.name for job in job_queue.jobs()]:
+        return
+    job_queue.run_daily(
+        yesterday_stocks_job,
+        time=datetime.time(hour=7),
+        name=job_title,
+    )
 
-def yesterday_stocks(update, context):
-    msg = f'Ваши фин. показатели по итогу прошлого дня:\n\n'
+
+def format_ticker_stats(ticker: str, amount: int, open_price: float, close_price: float) -> str:
+    diff = close_price - open_price
+    revenue = diff * amount
+
+    if revenue > 0:
+        result = 'заработали'
+    else:
+        result = 'потеряли'
+
+    return dedent(f'''
+        {ticker} ({amount})
+        открылись по {open_price:,.1f}$
+        закрылись по {close_price:,.1f}$
+        выручка с акции: {diff:,.1f}$
+        учитывая количество, {result} {revenue:,.1f}$
+    ''')
+
+
+def current_stocks(update: Update, context: CallbackContext) -> None:
+    msg = f'Ваши фин. показатели на текущий момент:\n\n'
 
     day_revenue = 0
 
     for ticker, amount in portfolio.items():
-        yahoo_ticker_handler = yfinance.Ticker(ticker)
-        history = yahoo_ticker_handler.history(period="1mo")
-        stock_open = float(history['Open'][-2])
-        stock_close = float(history['Close'][-2])
-        diff = stock_close - stock_open
-        
+        open_price, close_price = get_current_trading_prices(ticker)
+        diff = close_price - open_price
         revenue = diff * amount
-
         day_revenue += revenue
-
-        if revenue > 0:
-            result = 'заработали'
-        else:
-            result = 'потеряли'
-
-        msg += dedent(f'''
-            {ticker} ({amount})
-            открылись по {stock_open:,.1f}$
-            закрылись по {stock_close:,.1f}$
-            выручка с акции сегодня: {diff:,.1f}$
-            учитывая количество, {result} {revenue:,.1f}$
-        ''')
+        msg += format_ticker_stats(ticker, amount, open_price, close_price)
     msg += dedent(f'''
         Итоговая разница за день: {day_revenue:,.1f}$
     ''')
     update.message.reply_text(msg)
 
 
+def yesterday_stocks_job(context: CallbackContext) -> None:
+    msg = f'Ваши фин. показатели по итогу прошлого дня:\n\n'
+
+    day_revenue = 0
+
+    for ticker, amount in portfolio.items():
+        open_price, close_price = get_yesterday_trading_prices(ticker)
+        diff = close_price - open_price
+        revenue = diff * amount
+        day_revenue += revenue
+        msg += format_ticker_stats(ticker, amount, open_price, close_price)
+    msg += dedent(f'''
+        Итоговая разница за день: {day_revenue:,.1f}$
+    ''')
+    context.bot.message.reply_text(msg)
+
+
 if __name__ == '__main__':
     token = env('TELEGRAM_BOT_TOKEN')
     updater = Updater(token)
     dispatcher = updater.dispatcher
+    job_queue = updater.job_queue
 
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("yesterday_stocks", yesterday_stocks))
+    dispatcher.add_handler(CommandHandler("current_stocks", current_stocks))
 
     updater.start_polling()
     updater.idle()
---
+++ stocks.py
@@ -0,0 +1,20 @@
+from typing import Tuple
+
+import yfinance
+
+
+def get_yesterday_trading_prices(ticker: str) -> Tuple[float, float]: 
+    yahoo_ticker_handler = yfinance.Ticker(ticker)
+    print(yahoo_ticker_handler)
+    history = yahoo_ticker_handler.history(period="1mo")
+    open_price = float(history['Open'][-2])
+    close_price = float(history['Close'][-2])
+    return open_price, close_price
+
+
+def get_current_trading_prices(ticker: str) -> Tuple[float, float]:
+    yahoo_ticker_handler = yfinance.Ticker(ticker)
+    history = yahoo_ticker_handler.history(period="1d")
+    open_price = float(history['Open'][-1])
+    close_price = float(history['Close'][-1])
+    return open_price, close_price
